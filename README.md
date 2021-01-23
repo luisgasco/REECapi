59665c3edab48881c37f732fd4dea2bfd58e4960
luisgasco <luisgascosanchez@gmail.com>
2021-01-23 22:53:42 +0100

    Version Pypt

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
-
+Documentation to get access to REEC 
 
 https://sede.aemps.gob.es/docs/Manual-Interaccion-REEC-Servicio-Extraccion-Datos-v1.pdf
---
+++ tests/test_getters.py
@@ -0,0 +1,12 @@
+from reecrawler import getters
+
+def test_get_trials_list():
+    assert type(getters.get_trials_list(from_date = "01-01-2021")["estudio"]) is list 
+    assert type(getters.get_trials_list(from_date = "11-11-2020", to_date = "11-12-2020")["estudio"]) is list
+
+def test_get_trials_list():
+    # TO BE DEFINED
+
+
+    
+        
